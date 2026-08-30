0aa2f99dcafd677a6136aa931db0eb5f980186ac
[Cole Northway] <[cenorthway289@gmail.com]>
2026-08-29 23:36:46 -0500

    Update README.md

--- lib/hash_map/hash_map.rb
+++ lib/hash_map/hash_map.rb
@@ -4,10 +4,11 @@ class HashMap
   def initialize
     @capacity = 16
     @load_factor = 0.75
+    @buckets = Array.new(16) { Array.new }
   end
 
   private
 
-  attr_accessor :capacity
+  attr_accessor :capacity, :buckets
   attr_reader :load_factor
 end
