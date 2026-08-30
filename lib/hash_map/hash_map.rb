4a4f0d31666081d278b43c06acbda861e6b2e02f
[Cole Northway] <[cenorthway289@gmail.com]>
2026-08-30 00:21:29 -0500

    Make Tests for HashMap#set

--- lib/hash_map/hash_map.rb
+++ lib/hash_map/hash_map.rb
@@ -9,6 +9,15 @@ class HashMap
 
   private
 
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code % capacity
+  end
+
   attr_accessor :capacity, :buckets
   attr_reader :load_factor
 end
--- spec/hash_map_spec.rb
+++ spec/hash_map_spec.rb
@@ -5,7 +5,20 @@
 require_relative '../lib/hash_map//hash_map'
 
 RSpec.describe LinkedList do
+  describe '#set' do
+    it 'Should have the value inserted after setting' do
+      map = HashMap.new
+      map.set('key', 'value')
+      expect(map.get('key')).to eql('value')
+    end
 
+    it 'Should have the new value after setting and overriding' do
+      map = HashMap.new
+      map.set('key', 'value')
+      map.set('key', 'something')
+      expect(map.get('key')).to eql('something')
+    end
+  end
 end
 
 
